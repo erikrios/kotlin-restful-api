979f2ab04cf18ac7b7ae05d228dd30be5bd1826d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-03 20:14:08 +0700

    Add update method

--- src/main/kotlin/io/erikrios/github/kotlinrestfulapi/service/ProductService.kt
+++ src/main/kotlin/io/erikrios/github/kotlinrestfulapi/service/ProductService.kt
@@ -2,8 +2,10 @@ package io.erikrios.github.kotlinrestfulapi.service
 
 import io.erikrios.github.kotlinrestfulapi.model.CreateProductRequest
 import io.erikrios.github.kotlinrestfulapi.model.ProductResponse
+import io.erikrios.github.kotlinrestfulapi.model.UpdateProductRequest
 
 interface ProductService {
     fun create(createProductRequest: CreateProductRequest): ProductResponse
     fun get(id: String): ProductResponse
+    fun update(id: String, updateProductRequest: UpdateProductRequest): ProductResponse
 }
