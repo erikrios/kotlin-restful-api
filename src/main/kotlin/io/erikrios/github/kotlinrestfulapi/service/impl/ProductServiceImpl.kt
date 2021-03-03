3ec1bc16b62ff482382725188d48e7b0641ac2ee
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-03 12:51:03 +0700

    Create ProductService.kt

---
+++ src/main/kotlin/io/erikrios/github/kotlinrestfulapi/service/impl/ProductServiceImpl.kt
@@ -0,0 +1,35 @@
+package io.erikrios.github.kotlinrestfulapi.service.impl
+
+import io.erikrios.github.kotlinrestfulapi.entity.Product
+import io.erikrios.github.kotlinrestfulapi.model.CreateProductRequest
+import io.erikrios.github.kotlinrestfulapi.model.ProductResponse
+import io.erikrios.github.kotlinrestfulapi.repository.ProductRepository
+import io.erikrios.github.kotlinrestfulapi.service.ProductService
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class ProductServiceImpl(val productRepository: ProductRepository) : ProductService {
+
+    override fun create(createProductRequest: CreateProductRequest): ProductResponse {
+        val product = Product(
+            id = createProductRequest.id,
+            name = createProductRequest.name,
+            price = createProductRequest.price,
+            quantity = createProductRequest.quantity,
+            createdAt = Date(),
+            updatedAt = null
+        )
+
+        productRepository.save(product)
+
+        return ProductResponse(
+            id = product.id,
+            name = product.name,
+            price = product.price,
+            quantity = product.quantity,
+            createdAt = product.createdAt,
+            updatedAt = product.updatedAt
+        )
+    }
+}
