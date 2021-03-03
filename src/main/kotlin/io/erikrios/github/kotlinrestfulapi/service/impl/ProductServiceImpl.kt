778f2471fbd24a0906ffd84e4643eabc75728944
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-03 19:05:44 +0700

    Add get method to get a product

--- src/main/kotlin/io/erikrios/github/kotlinrestfulapi/service/impl/ProductServiceImpl.kt
+++ src/main/kotlin/io/erikrios/github/kotlinrestfulapi/service/impl/ProductServiceImpl.kt
@@ -1,11 +1,13 @@
 package io.erikrios.github.kotlinrestfulapi.service.impl
 
 import io.erikrios.github.kotlinrestfulapi.entity.Product
+import io.erikrios.github.kotlinrestfulapi.error.NotFoundException
 import io.erikrios.github.kotlinrestfulapi.model.CreateProductRequest
 import io.erikrios.github.kotlinrestfulapi.model.ProductResponse
 import io.erikrios.github.kotlinrestfulapi.repository.ProductRepository
 import io.erikrios.github.kotlinrestfulapi.service.ProductService
 import io.erikrios.github.kotlinrestfulapi.validation.ValidationUtil
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -29,6 +31,17 @@ class ProductServiceImpl(
 
         productRepository.save(product)
 
+        return convertProductToProductResponse(product)
+    }
+
+    override fun get(id: String): ProductResponse {
+        val product = productRepository.findByIdOrNull(id)
+        product?.let {
+            return convertProductToProductResponse(it)
+        } ?: throw NotFoundException()
+    }
+
+    private fun convertProductToProductResponse(product: Product): ProductResponse {
         return ProductResponse(
             id = product.id,
             name = product.name,
