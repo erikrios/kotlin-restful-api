2b73814d363ca499e4d9c4999e9a08f9a6ad871a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-03 18:56:30 +0700

    Add response status

--- src/main/kotlin/io/erikrios/github/kotlinrestfulapi/controller/ErrorController.kt
+++ src/main/kotlin/io/erikrios/github/kotlinrestfulapi/controller/ErrorController.kt
@@ -3,12 +3,13 @@ package io.erikrios.github.kotlinrestfulapi.controller
 import io.erikrios.github.kotlinrestfulapi.model.WebResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import javax.validation.ConstraintViolationException
 
 @RestControllerAdvice
 class ErrorController {
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = [ConstraintViolationException::class])
     fun validationHandler(constraintViolationException: ConstraintViolationException): WebResponse<String> {
         return WebResponse(
