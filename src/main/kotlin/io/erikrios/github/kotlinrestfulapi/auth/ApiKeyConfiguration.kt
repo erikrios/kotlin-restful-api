7217bf98b35547b5d168e1a9faae58589b5aa8b8
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-03 22:40:46 +0700

    Create ApiKeyConfiguration.kt

---
+++ src/main/kotlin/io/erikrios/github/kotlinrestfulapi/auth/ApiKeyConfiguration.kt
@@ -0,0 +1,15 @@
+package io.erikrios.github.kotlinrestfulapi.auth
+
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Component
+class ApiKeyConfiguration(val apiKeyInterceptor: ApiKeyInterceptor) : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        super.addInterceptors(registry)
+
+        registry.addWebRequestInterceptor(apiKeyInterceptor)
+    }
+}
