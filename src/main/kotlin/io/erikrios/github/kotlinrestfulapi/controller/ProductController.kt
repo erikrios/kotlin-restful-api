2a4ae55bccb4ff7e617611e81d4e882cb32fe2bc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-03 20:47:35 +0700

    Add an endpoint to update a product

--- src/main/kotlin/io/erikrios/github/kotlinrestfulapi/controller/ProductController.kt
+++ src/main/kotlin/io/erikrios/github/kotlinrestfulapi/controller/ProductController.kt
@@ -2,6 +2,7 @@ package io.erikrios.github.kotlinrestfulapi.controller
 
 import io.erikrios.github.kotlinrestfulapi.model.CreateProductRequest
 import io.erikrios.github.kotlinrestfulapi.model.ProductResponse
+import io.erikrios.github.kotlinrestfulapi.model.UpdateProductRequest
 import io.erikrios.github.kotlinrestfulapi.model.WebResponse
 import io.erikrios.github.kotlinrestfulapi.service.ProductService
 import org.springframework.http.HttpStatus
@@ -32,6 +33,24 @@ class ProductController(val productService: ProductService) {
     )
     fun getProduct(@PathVariable("idProduct") id: String): WebResponse<ProductResponse> {
         val productResponse = productService.get(id)
+
+        return WebResponse(
+            code = HttpStatus.OK.value(),
+            status = HttpStatus.OK.reasonPhrase,
+            data = productResponse
+        )
+    }
+
+    @PutMapping(
+        value = ["/api/products/{idProduct}"],
+        produces = [MediaType.APPLICATION_JSON_VALUE],
+        consumes = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun updateProduct(
+        @PathVariable("idProduct") id: String,
+        @RequestBody body: UpdateProductRequest
+    ): WebResponse<ProductResponse> {
+        val productResponse = productService.update(id, body)
 
         return WebResponse(
             code = HttpStatus.OK.value(),
