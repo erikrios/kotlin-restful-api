f838b743862e3c81e0cc9cf2e1d79550c6ca7f76
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-03 22:08:16 +0700

    Create ApiKey.kt

---
+++ src/main/kotlin/io/erikrios/github/kotlinrestfulapi/entity/ApiKey.kt
@@ -0,0 +1,13 @@
+package io.erikrios.github.kotlinrestfulapi.entity
+
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "api_keys")
+data class ApiKey(
+
+    @Id
+    val id: String
+)
