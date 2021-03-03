1b05ac72d0c0a6b9886905b6759d43fb119f99b8
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-03 12:29:03 +0700

    Create Products.kt

---
+++ src/main/kotlin/io/erikrios/github/kotlinrestfulapi/entity/Products.kt
@@ -0,0 +1,29 @@
+package io.erikrios.github.kotlinrestfulapi.entity
+
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "products")
+data class Products(
+    @Id
+    val id: String,
+
+    @Column(name = "name")
+    val name: String,
+
+    @Column(name = "price")
+    val price: Long,
+
+    @Column(name = "quantity")
+    val quantity: Int,
+
+    @Column(name = "created_at")
+    val createdAt: Date,
+
+    @Column(name = "updated_at")
+    val updatedAt: Date?
+)
