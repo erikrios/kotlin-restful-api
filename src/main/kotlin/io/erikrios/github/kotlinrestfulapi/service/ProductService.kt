6bb47ec8dd53d4d6b6333925c2974419f3e7b1ad
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-03 21:20:01 +0700

    Add delete method

--- src/main/kotlin/io/erikrios/github/kotlinrestfulapi/service/ProductService.kt
+++ src/main/kotlin/io/erikrios/github/kotlinrestfulapi/service/ProductService.kt
@@ -8,4 +8,5 @@ interface ProductService {
     fun create(createProductRequest: CreateProductRequest): ProductResponse
     fun get(id: String): ProductResponse
     fun update(id: String, updateProductRequest: UpdateProductRequest): ProductResponse
+    fun delete(id: String): String
 }
