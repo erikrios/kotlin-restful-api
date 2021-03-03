99fc1573b318b922084bcf42b4093db9f6c0018f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-03 10:44:50 +0700

    Initial commit

---
+++ src/main/kotlin/io/erikrios/github/kotlinrestfulapi/KotlinRestfulApiApplication.kt
@@ -0,0 +1,11 @@
+package io.erikrios.github.kotlinrestfulapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestfulApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestfulApiApplication>(*args)
+}
---
+++ src/test/kotlin/io/erikrios/github/kotlinrestfulapi/KotlinRestfulApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.erikrios.github.kotlinrestfulapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestfulApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
