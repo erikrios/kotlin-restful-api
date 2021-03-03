433ff8430cd7c7bba67be5c46855e0848c6b91b7
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-03 21:39:59 +0700

    Add a method to get list of products

--- src/main/kotlin/io/erikrios/github/kotlinrestfulapi/service/ProductService.kt
+++ src/main/kotlin/io/erikrios/github/kotlinrestfulapi/service/ProductService.kt
@@ -1,12 +1,18 @@
 package io.erikrios.github.kotlinrestfulapi.service
 
 import io.erikrios.github.kotlinrestfulapi.model.CreateProductRequest
+import io.erikrios.github.kotlinrestfulapi.model.ListProductRequest
 import io.erikrios.github.kotlinrestfulapi.model.ProductResponse
 import io.erikrios.github.kotlinrestfulapi.model.UpdateProductRequest
 
 interface ProductService {
     fun create(createProductRequest: CreateProductRequest): ProductResponse
+
     fun get(id: String): ProductResponse
+
     fun update(id: String, updateProductRequest: UpdateProductRequest): ProductResponse
+
     fun delete(id: String): String
+
+    fun list(listProductRequest: ListProductRequest): List<ProductResponse>
 }
